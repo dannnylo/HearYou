92bfa3ffd12c761a123737a5e01c2b420f56e12d
Danilo Jeremias da Silva <daniloj.dasilva@gmail.com>
2017-11-23 23:56:56 -0200

    First test of parse podcast data

---
+++ src/api/RSSParser.js
@@ -0,0 +1,49 @@
+var FeedParser = require('feedparser');
+var request = require('request');
+var PodcastItem = require('./PodcastItem');
+
+// module.exports =
+class RSSParser {
+  constructor(feedId, url) {
+    this.feedId = feedId;
+    this.url = url;
+  }
+
+  getItems(callback){
+    var feedparser = new FeedParser([]);
+    var req = request(this.url);
+
+    req.on('error', function (error) {
+      // handle any request errors
+    });
+
+    req.on('response', function (res) {
+      var stream = this;
+
+      if (res.statusCode !== 200) {
+        this.emit('error', new Error('Bad status code'));
+      }
+      else {
+        stream.pipe(feedparser);
+      }
+    });
+
+    feedparser.on('error', function (error) {
+      // always handle errors
+    });
+
+    feedparser.on('readable', function () {
+      var stream = this;
+      var meta = this.meta;
+      var item;
+      while (item = stream.read()) {
+        callback(new PodcastItem(item))
+      }
+    });
+
+  }
+}
+
+new RSSParser('1', 'http://www.mundofreak.com.br/feed/podcast/').getItems(function(item){
+  console.info(item.getFileUrl())
+})
